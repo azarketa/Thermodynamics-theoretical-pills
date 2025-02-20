7f6beee745c66b4965fb588cd7371c525c75e03c
Ander Zarketa-Astigarraga <azarketa@mondragon.edu>
2025-02-20 17:51:05 +0100

    Add files via upload

--- conf.py
+++ conf.py
@@ -35,7 +35,7 @@ extensions = [
 ]
 
 # Configure MathJax for equation numbering and cross-references
-# mathjax_path = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.7/MathJax.js?config=TeX-MML-AM_CHTML'
+mathjax_path = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.7/MathJax.js?config=TeX-MML-AM_CHTML'
 
 templates_path = ['_templates']
 exclude_patterns = []
