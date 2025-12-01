432a95a5be92e797a76a6c590cc9e3fafe8d85e7
Ander Zarketa-Astigarraga <azarketa@mondragon.edu>
2025-12-01 08:33:30 +0100

    Files updated

--- source/_build/html/_static/custom.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("DOMContentLoaded", function () {
-    if (window.MathJax) {
-        MathJax.typesetPromise().then(() => {
-            console.log("MathJax equations rendered.");
-        }).catch((err) => console.error("MathJax error:", err));
-    } else {
-        console.error("MathJax not loaded.");
-    }
-});
